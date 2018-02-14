83a46a0eef2f72b2959e31700ea99e1d91724076
ppescher <p.messina@robotechsrl.com>
2018-02-14 16:34:28 +0100

    Fix implicit conversion to double precision floating point

--- cores/arduino/Stream.cpp
+++ cores/arduino/Stream.cpp
@@ -162,7 +162,7 @@ float Stream::parseFloat(LookaheadMode lookahead, char ignore)
   bool isFraction = false;
   long value = 0;
   int c;
-  float fraction = 1.0;
+  float fraction = 1.0f;
 
   c = peekNextDigit(lookahead, true);
     // ignore non numeric leading characters
@@ -179,7 +179,7 @@ float Stream::parseFloat(LookaheadMode lookahead, char ignore)
     else if(c >= '0' && c <= '9')  {      // is c a digit?
       value = value * 10 + c - '0';
       if(isFraction)
-         fraction *= 0.1;
+         fraction *= 0.1f;
     }
     read();  // consume the character we got with peek
     c = timedPeek();
