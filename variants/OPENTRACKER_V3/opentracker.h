ccec7ee9762ed102b823d4077337fe15b260e370
ppescher <p.messina@robotechsrl.com>
2018-02-15 15:39:15 +0100

    Complete definitions for OpenTracker hardware

--- variants/OPENTRACKER_V3/opentracker.h
+++ variants/OPENTRACKER_V3/opentracker.h
@@ -17,78 +17,75 @@
 #define PIN_C_PWR_GSM   PE2
 #define PIN_C_KILL_GSM  PD3
 
-//#define PIN_STANDBY_GPS n/a
 #define PIN_RESET_GPS   PH3
+//#define PIN_STANDBY_GPS NC
+//#define PIN_S_PPS_GPS   NC
 
 #define PIN_C_IN1_PD    PE8
 #define PIN_C_IN2_PD    PE7
+#define PIN_C_IN3_PD    PD14
+#define PIN_C_IN4_PD    PD12
+
 #define ANALOG_VREF     2.50f
+#define ANALOG_SCALE    13.0154525f
+#define ANALOG_SCALE_LOW 2.0f
 
 #define AIN_EXT_IN1     PA1
 #define AIN_EXT_IN2     PA3
 #define AIN_EXT_IN3     PC0
 #define AIN_EXT_IN4     PC1
 
-#define MODEM_VBAT      4.06f
-#define MODEM_UG96      1
+#define AIN_EXT_AN2     PC2
+#define AIN_EXT_AN1     PC3
 
-/* OpenTracker PINS 
+#define PIN_EXT_IN1     PA1
+#define PIN_EXT_IN2     PA3
+#define PIN_EXT_IN3     PC0
+#define PIN_EXT_IN4     PC1
 
-#define PIN_EXT_RX      0
-#define PIN_EXT_TX      1
-#define PIN_S_PPS_GPS   2
-#define PIN_EXT_PA21    6
-#define PIN_EXT_PA20    7
-#define PIN_EXT_TXD2    14
-#define PIN_EXT_RXD2    15
-#define PIN_TX1_GSM     16
-#define PIN_RX1_GSM     17
-#define PIN_TX0_GPS     18
-#define PIN_RX0_GPS     19
-#define PIN_EXT_SDA     20
-#define PIN_EXT_SCL     21
-#define PIN_EXT_IN1     28
-#define PIN_EXT_IN2     29
-#define PIN_EXT_PA22    30
-#define PIN_EXT_PA23    31
-#define PIN_CANRX       32
-#define PIN_CANTX       33
-#define PIN_EXT_MISO    34
-#define PIN_EXT_MOSI    35
-#define PIN_EXT_SCK     36
-#define PIN_EXT_NS0     37
-#if !defined(OPENTRACKER_HW_REV) || (OPENTRACKER_HW_REV < 0x0240)
-// HW Revision 2.3 (and earlier)
-#define PIN_C_REBOOT    4
-#define PIN_RING_GSM    22
-#define PIN_GSM_VDD_EXT 24
-#define ANALOG_VREF     3.40
-#define MODEM_VBAT      4.13
-#define MODEM_M95       1
-#else
-// HW Revision 2.4 (and later)
-// dropped "REBOOT" pin and "GSM VDD" (unused)
-// moved "RING" to wakeup capable pins
-// added "INx_PD" pull-down enable pins, to change analog input range
-#define PIN_RING_GSM    24
-#define PIN_C_IN1_PD    38
-#define PIN_C_IN2_PD    39
-#define ANALOG_VREF     3.31
-#if (OPENTRACKER_HW_REV == 0x024A)
-#define MODEM_VBAT      4.10
-#define MODEM_UG96      1
-#else
-#define MODEM_VBAT      4.36
-#define MODEM_M95       1
-#endif
-#endif
+#define PIN_EXT_AN2     PC2
+#define PIN_EXT_AN1     PC3
 
-#define AIN_S_INLEVEL   49
-#define AIN_EXT_IN1     28
-#define AIN_EXT_IN2     29
-#define AIN_EXT_PA22    30
-#define AIN_EXT_PA23    31
-*/
+#define PIN_EXT_TXD2    PD8
+#define PIN_EXT_RXD2    PD9
+#define PIN_EXT_TX      PB11
+#define PIN_EXT_RX      PB10
+#define PIN_TX1_GSM     PD5
+#define PIN_RX1_GSM     PD6
+#define PIN_TX0_GPS     PA9
+#define PIN_RX0_GPS     PA10
+
+#define PIN_EXT_SDA     PB7
+#define PIN_EXT_SCL     PB6
+
+#define PIN_CANRX       PB8
+#define PIN_CANTX       PB9
+
+#define PIN_EXT_MISO    PB14
+#define PIN_EXT_MOSI    PB15
+#define PIN_EXT_SCK     PB13
+#define PIN_EXT_NS0     PB12
+
+#define PIN_S_BUTTON    PE6
+#define PIN_C_POWERMODE PE4
+#define PIN_C_5V_ENABLE PE5
+#define PIN_EXT_AUDIO   PD4
+#define PIN_C_ANTON     PD0
+#define PIN_S_USBVBUS   PC7
+
+#define PIN_EXT_INT     PD15
+#define PIN_EXT_PWM     PD13
+#define PIN_EXT_RST     PC4
+#define PIN_EXT_DAC1    PA4
+#define PIN_EXT_DAC2    PA5
+
+#define PIN_S_ACC_INT1  PD11
+#define PIN_C_OUT_ENA   PC6
+
+#define PIN_C_ACC_CS    PD10
+#define PIN_C_OUT_CS    PE12
+
+#define MODEM_VBAT      4.10f
 
 #ifndef MODEM_UG96
 #define MODEM_UG96      0
